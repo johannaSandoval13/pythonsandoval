ddf189201ce43acd69706e79fb435241bb6cf789
Johanna <johanna.sandoval634@gmail.com>
2023-05-10 17:19:38 -0500

    Avance :)

--- pythonjohanna/funciones/miscelanea.py
+++ pythonjohanna/funciones/miscelanea.py
@@ -1,5 +1,5 @@
 import random
-
+tam=(random.randint(10,15))
 def llenarLista(tam,rango):
     lista=[]
     lista=[random.randrange(rango) for i in range(tam)]
@@ -27,11 +27,28 @@ def menorLista(lista):
             Nmenor=num
     return Nmenor
 
+def ordenascendente (lista,tam):
+    for i in range(len(lista)):
+     for j in range(i+1,tam):
+        if lista[i]>lista[j]:
+            aux=lista[i]
+            lista[i]=lista[j]
+            lista[j]=aux
+    return lista
 
 l1=llenarLista(10,20)
+l2=llenarLista(10,20)
+print("Lista 1: ")
 print(l1)
 print(sumaLista(l1))
 print(promedioLista(l1))
 print(mayorLista(l1))
 print(menorLista(l1))
-print(ordenALista(l1))
+print(ordenascendente(l1,tam))
+print("Lista 2: ")
+print(l2)
+print(sumaLista(l2))
+print(promedioLista(l2))
+print(mayorLista(l2))
+print(menorLista(l2))
+print(ordenascendente(l2,tam))
