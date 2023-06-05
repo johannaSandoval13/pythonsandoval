82bfe5abeed49c6cb7b5dfef5a26890f94f2523c
Johanna <johanna.sandoval634@gmail.com>
2023-06-05 13:53:57 -0500

    prueba :)

--- pythonjohanna/POO/persona.py
+++ pythonjohanna/POO/persona.py
@@ -13,19 +13,49 @@ class Persona:
         return self.__documento
     def setDocumento(self,documento):
         self.__documento=documento
+
+    def añadeCursos(self):
+        cursos = []
+        while True:
+            c = input("Añada los cursos , escriba 'salir' para finalizar: ")
+            if c == "salir":
+                break
+            cursos.append(c)
+        self.__cursos = cursos
         
-    def añadeCursos(self,cursos):
-        while c != "salir":
-            c=input("Añada los cursos a los que pertenece, escriba 'salir' para finalizar: ")
-            c.append(cursos)
-        return(cursos)
     def muestraDatos(self):
         print("Los datos de la persona son: ")
-        return self.__nombre, self.__documento, self.__cursos
+        return ("-Nombre:", self.__nombre, "-Documento:", self.__documento, "-Cursos:", self.__cursos)
         
+    def eliminaCursos(self):
+        while True:
+            e = input("escriba el indice a eliminar, escriba 'salir' para finalizar: ")
+            if e == "salir":
+                break
+            index = int(e)
+            del self.__cursos[index]
+    def modificaCursos(self):
+        cursos = []
+        while True:
+            m = input("escriba el indice a modificar, escriba 'salir' para finalizar: ")
+            if m == "salir":
+                break
+            index = int(m)
+            nvCurso = input("Ingrese el nuevo curso: ")
+            self.__cursos[index] = nvCurso
 
-p=Persona("Timmy",678,2)
+
+
+p = Persona("Timmy", 678, [2])
+p.añadeCursos()
+print(p.muestraDatos())
+p.eliminaCursos()
+p.modificaCursos()
 print(p.muestraDatos())
 
-q=Persona("Billy",910,1)
+q = Persona("Billy", 910, [1])
+q.añadeCursos()
+print(q.muestraDatos())
+q.eliminaCursos()
+q.modificaCursos()
 print(q.muestraDatos())
